5d57c397e1cddd1c058be5c3f904fd34f972daf8
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2017-09-22 11:15:43 +0300

    Load next scene functionality

--- Assets/_Scripts/ToScene.cs
+++ Assets/_Scripts/ToScene.cs
@@ -6,28 +6,33 @@ using UnityEngine.SceneManagement;
 public class ToScene : MonoBehaviour {
 	public GameObject guiObject;
 	public string LoadLevel;
+	private bool insideTrigger = false;
 
 	// Use this for initialization
 	void Start () {
 		guiObject.SetActive (false);
 	}
 
-	void OnTriggerEnter2D (Collider2D other){
-		if (other.gameObject.tag == "Player") 
-		{
-			
-			guiObject.SetActive (true);
-
-			if (guiObject.activeInHierarchy == true && Input.GetMouseButtonDown (0)) {
-				Debug.Log ("pressed mouse button, load scene");
-				SceneManager.LoadScene (LoadLevel);
-				Debug.Log ("scene loaded");
-			}
+	void Update(){
+		if(Input.GetMouseButtonDown(0) && insideTrigger){
+			//Debug.Log("Load next scene");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		}
 	}
-	void OnTriggerExit()
+
+	void OnTriggerEnter2D (Collider2D other){
+		if (other.gameObject.tag == "Player") {
+			guiObject.SetActive (true);
+			insideTrigger = true;
+			//Debug.Log("inideTrigger: " + insideTrigger);
+			
+		}
+	}
+	void OnTriggerExit2D()
 	{
 		guiObject.SetActive (false);
+		insideTrigger = false;
+		//Debug.Log("inideTrigger: " + insideTrigger);
 	}
 
 }
