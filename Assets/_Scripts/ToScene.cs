ac1f83c736e9652456c0ca72aba5c2bb54c263a4
Wiluski <ville.vainio@metropolia.com>
2017-09-25 20:26:34 +0300

    Add small chances to scene Test2 and script ToScene

--- Assets/_Scripts/ToScene.cs
+++ Assets/_Scripts/ToScene.cs
@@ -4,32 +4,33 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ToScene : MonoBehaviour {
-	public GameObject guiObjectFor;
+	public GameObject guiObject;
 	//public GameObject guiObjectBack;
 	//public string LoadLevel;
 	//private bool insideTrigger = false;
 
 	// Use this for initialization
 	void Start () {
-		guiObjectFor.SetActive (false);
+		guiObject.SetActive (false);
+		Vector2 pos = transform.position;
 		//guiObjectBack.SetActive (false);
 	}
 
 	void Update(){
-		if(Input.GetMouseButtonDown(0) && guiObjectFor.activeInHierarchy == true /*insideTrigger*/){
+		if(Input.GetMouseButtonDown(0) && guiObject.activeInHierarchy == true && transform.position.x > 5 /*insideTrigger*/){
 			//Debug.Log("Load next scene");
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		}
-		/*if(Input.GetMouseButtonDown(0) && guiObjectBack.activeInHierarchy == true insideTrigger){
+		if(Input.GetMouseButtonDown(0) && guiObject.activeInHierarchy == true && transform.position.x < 5 /*insideTrigger*/){
 			//Debug.Log("Load next scene");
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 		}
-		*/
+
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.tag == "Player") {
-			guiObjectFor.SetActive (true);
+			guiObject.SetActive (true);
 			//guiObjectBack.SetActive (true);
 			//insideTrigger = true;
 			//Debug.Log("inideTrigger: " + insideTrigger);
@@ -38,7 +39,7 @@ public class ToScene : MonoBehaviour {
 	}
 	void OnTriggerExit2D()
 	{
-		guiObjectFor.SetActive (false);
+		guiObject.SetActive (false);
 		//guiObjectBack.SetActive (false);
 		//insideTrigger = false;
 		//Debug.Log("inideTrigger: " + insideTrigger);
