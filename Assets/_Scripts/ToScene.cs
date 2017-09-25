110a2f6d8cfbae50a8e4ce84ef682b926be001ed
Wiluski <ville.vainio@metropolia.com>
2017-09-25 13:34:02 +0300

    Small test changes to script, new collision trigger on Test2

--- Assets/_Scripts/ToScene.cs
+++ Assets/_Scripts/ToScene.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ToScene : MonoBehaviour {
-	public GameObject guiObject;
-	public string LoadLevel;
-	private bool insideTrigger = false;
+	public GameObject guiObjectFor;
+	//public GameObject guiObjectBack;
+	//public string LoadLevel;
+	//private bool insideTrigger = false;
 
 	// Use this for initialization
 	void Start () {
-		guiObject.SetActive (false);
+		guiObjectFor.SetActive (false);
+		//guiObjectBack.SetActive (false);
 	}
 
 	void Update(){
-		if(Input.GetMouseButtonDown(0) && insideTrigger){
+		if(Input.GetMouseButtonDown(0) && guiObjectFor.activeInHierarchy == true /*insideTrigger*/){
 			//Debug.Log("Load next scene");
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 		}
+		/*if(Input.GetMouseButtonDown(0) && guiObjectBack.activeInHierarchy == true insideTrigger){
+			//Debug.Log("Load next scene");
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+		}
+		*/
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.tag == "Player") {
-			guiObject.SetActive (true);
-			insideTrigger = true;
+			guiObjectFor.SetActive (true);
+			//guiObjectBack.SetActive (true);
+			//insideTrigger = true;
 			//Debug.Log("inideTrigger: " + insideTrigger);
 			
 		}
 	}
 	void OnTriggerExit2D()
 	{
-		guiObject.SetActive (false);
-		insideTrigger = false;
+		guiObjectFor.SetActive (false);
+		//guiObjectBack.SetActive (false);
+		//insideTrigger = false;
 		//Debug.Log("inideTrigger: " + insideTrigger);
 	}
 
