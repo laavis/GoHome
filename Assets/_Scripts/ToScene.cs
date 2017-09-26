9ac4c6ac38d4a24cf1380658654502fc687606a1
Wiluski <ville.vainio@metropolia.com>
2017-09-26 12:26:23 +0300

    testing on ToScene script

--- Assets/_Scripts/ToScene.cs
+++ Assets/_Scripts/ToScene.cs
@@ -5,14 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class ToScene : MonoBehaviour {
 	public GameObject guiObject;
+	public GameObject Player;
+	//public float pX;
+	//public float pY;
 	//public GameObject guiObjectBack;
 	//public string LoadLevel;
 	//private bool insideTrigger = false;
-
 	// Use this for initialization
 	void Start () {
 		guiObject.SetActive (false);
-		Vector2 pos = transform.position;
+		/*if (PlayerPrefs.GetInt ("Saved") == 1) {
+			PlayerPrefs.GetFloat ("p_x");
+			PlayerPrefs.GetFloat ("p_y");
+
+			transform.position = new Vector2 (pX, pY);
+			PlayerPrefs.SetInt ("Saved", 0);
+			PlayerPrefs.Save ();
+			Debug.Log ("position saved");
+		}*/
+		//Vector2 pos = transform.position;
 		//guiObjectBack.SetActive (false);
 	}
 
@@ -24,13 +35,18 @@ public class ToScene : MonoBehaviour {
 		if(Input.GetMouseButtonDown(0) && guiObject.activeInHierarchy == true && transform.position.x < 5 /*insideTrigger*/){
 			//Debug.Log("Load next scene");
 			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
-		}
+			gameObject.tag = "Player";
+			transform.position = new Vector2 (4.0f, -1.85f);
 
+		}
 	}
 
 	void OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.tag == "Player") {
 			guiObject.SetActive (true);
+			/*if (transform.position.x < 5) {
+				other.transform.position = new Vector2 (4.0f, -1.85f);
+			}*/
 			//guiObjectBack.SetActive (true);
 			//insideTrigger = true;
 			//Debug.Log("inideTrigger: " + insideTrigger);
