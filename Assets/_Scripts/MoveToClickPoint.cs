ab50011baa09ebc91e99abcc4dacb4c532a978dc
laavis <sara.suviranta@gmail.com>
2017-09-30 16:47:40 +0300

    Player now moves via MoveToClicPoint.cs. Animations are working but stopping the movement while inventory is open is still in process

--- Assets/_Scripts/MoveToClickPoint.cs
+++ Assets/_Scripts/MoveToClickPoint.cs
@@ -4,21 +4,55 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class MoveToClickPoint : MonoBehaviour {
-	
+	Animator anim;
+	private SpriteRenderer playerSpriteRend;
+	public Transform player;
+	private static bool playerExists;
 	private float speed = 10f;
 	private Vector2 target;
 
+	public bool isMoving = false;	
+
 	void Start(){
 		target = transform.position;
+		anim = GetComponent<Animator>();
+		playerSpriteRend = GetComponent<SpriteRenderer>();
+
+		if (!playerExists) {
+			playerExists = true;
+			DontDestroyOnLoad (transform.gameObject);
+		} else {
+			Destroy (gameObject);
+		}
 	}
 
 	void FixedUpdate(){
-		if (!GameManager.isPaused) Move();
+		//Don't move if inventory or text box is active
+		if (!GameManager.isPaused){
+			Move();
+		} 
+
+		//If target position the left side of the player, flip the player so it will be facing towards left
+		if(target.x < player.position.x){
+			playerSpriteRend.flipX = true;	
+		
+		}else if(target.x > player.position.x){
+			playerSpriteRend.flipX = false;	
+		}
+
+		//Play walk animation until the player is reached the target position
+		if(isMoving == true && !GameManager.isPaused){
+			anim.Play("Walk");
+		}else{
+			anim.Play("Idle");
+		}
 	}
 
 	void Move(){
 			if(Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)) {
-
+			
+			isMoving = true;
+		
 			#if UNITY_EDITOR
 			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
@@ -27,9 +61,13 @@ public class MoveToClickPoint : MonoBehaviour {
 			#endif
 
 			target.y = transform.position.y;
-		}
-		transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
-		
+			
+			//If the player has reached the target position, stop moving. 
+			}else if(target.x == player.position.x){
+				isMoving = false;
+			}
+			//Move the player to the target position
+			transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
 	}
 }
 
