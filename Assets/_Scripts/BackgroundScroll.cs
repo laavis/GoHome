67baa8f2d1add295559ae51aaf06ef374385c494
laavis <sara.suviranta@gmail.com>
2017-10-06 12:44:51 +0300

    edited the colors of the main menu

---
+++ Assets/_Scripts/BackgroundScroll.cs
@@ -0,0 +1,15 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ 
+ public class BackgroundScroll : MonoBehaviour {
+	 
+     public float scrollSpeed = 0.5F;
+     public Renderer rend;
+     void Start() {
+         rend = GetComponent<Renderer>();
+     }
+     void Update() {
+         float offset = Time.time * scrollSpeed;
+         rend.material.SetTextureOffset("MainMenu_bg", new Vector2(offset, 0));
+     }
+ }
