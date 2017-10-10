aafd80a36fc37d32675732e8ef34b683640b20d1
joonasmkauppinen <joonasm.kauppinen@gmail.com>
2017-10-10 09:59:30 +0300

    Changed menu scene background's position to begin from the top

--- Assets/_Scripts/BackgroundScroll.cs
+++ Assets/_Scripts/BackgroundScroll.cs
@@ -6,6 +6,6 @@
 
  void Update () {
         Vector2 offset = new Vector2(0, Time.time * speed);
-        GetComponent<Renderer>().material.mainTextureOffset = offset;       
-    }﻿
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
  }
